9f22afb6f799d856d72822b547bffddef8dc9f72
Dipen Bhandari <dipenbhandari205@outlook.com>
2026-06-04 10:31:20 -0500

    write a logic to push commit files to S3 bucket

--- backend/config/aws-config.js
+++ backend/config/aws-config.js
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import AWS from "aws-sdk";
 
 AWS.config.update({region:"us-east-1"});
@@ -6,4 +9,4 @@ const s3 = new AWS.S3();
 
 const S3_BUCKET = "alltracksdipen";
 
-export default {s3,S3_BUCKET};
+export  {s3,S3_BUCKET};
--- backend/controllers/push.js
+++ backend/controllers/push.js
@@ -1,5 +1,36 @@
-async function pushRepo(){
-    console.log("Push command called");
+import {s3, S3_BUCKET } from "../config/aws-config.js";
+import fs from "fs/promises";
+import path from "path";
+async function pushRepo() {
+    const repoPath = path.resolve(process.cwd(), ".track");
+    const commitsPath = path.join(repoPath, "commits");
+
+    try {
+        const commitDirs = await fs.readdir(commitsPath);
+        for (const commitDir of commitDirs) {
+            const commitPath = path.join(commitsPath, commitDir);
+            const files = await fs.readdir(commitPath);
+            for (const file of files) {
+                const filePath = path.join(commitPath, file);
+                const fileContent = await fs.readFile(filePath);
+
+                const params = {
+                    Bucket: S3_BUCKET,
+                    Key: `commits/${commitDir}/${file}`,
+                    Body: fileContent,
+                };
+
+                await s3.upload(params).promise();
+
+            }
+        }
+        console.log("Commits pushed to S3");
+    }
+    catch (err) {
+
+        console.error("Error pushing to S3:", err);
+    }
+
 
 };
 
