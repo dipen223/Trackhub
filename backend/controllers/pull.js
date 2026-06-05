32b1d76ccaba5f9e0ba75aa85ad5728c3ca44698
Dipen Bhandari <dipenbhandari205@outlook.com>
2026-06-05 10:51:53 -0500

    work on pull logic which pulls commits from S3 Bucekt

--- backend/controllers/pull.js
+++ backend/controllers/pull.js
@@ -1,5 +1,38 @@
+import {s3, S3_BUCKET } from "../config/aws-config.js";
+import fs from "fs/promises";
+import path from "path";
+
+
 async function pullRepo(){
-    console.log("Pull command called");
+    const repoPath = path.resolve(process.cwd(),".track");
+    const commitsPath = path.join(repoPath,"commits");
+    try{
+        const data = await s3.listObjectsV2({Bucket:S3_BUCKET,Prefix:"commits/"}).promise();
+
+        const objects = data.Contents;
+        for(const object of objects){
+            const key = object.Key;
+            const commitDir = path.join(commitsPath,path.dirname(key).split('/').pop());
+
+            await fs.mkdir(commitDir,{recursive:true});
+
+            const params = {
+                Bucket:S3_BUCKET,
+                Key:key,
+            };
+
+            const fileContent = await s3.getObject(params).promise();
+            // const filePath = path.join(commitDir,path.basename(key));
+            await fs.writeFile(path.join(repoPath,key),fileContent.Body);
+
+            console.log("All commits pulled from S3");
+
+        }
+    }catch(err){
+        console.error("Error pulling from s3");
+    }
+
+  
 }
 
 export default pullRepo;
